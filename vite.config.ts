58e31bc4b8240f84cb4a3b9891313b349652255f
InfSein <infsein@foxmail.com>
2026-01-09 21:13:36 +0800

    fix ghpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import PackageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? `/${PackageJson.name}/` : './',
   server: {
     host: "::",
     port: 3001,
